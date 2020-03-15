398ceb5ab37592def80894ac56f5d75d3131877f
TimurSamigulin <47005114+TimurSamigulin@users.noreply.github.com>
2020-03-16 00:46:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/medicalrecord/activity/DoctorActivity.kt
@@ -0,0 +1,42 @@
+package com.example.medicalrecord.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.example.medicalrecord.R
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_doctor.*
+import kotlinx.android.synthetic.main.toolbar.*
+
+class DoctorActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor)
+        setSupportActionBar(toolbar)
+        setTitle(R.string.doctor)
+
+
+        val toggle = ActionBarDrawerToggle(this, doctor_drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        doctor_drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        doctor_nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_menu_disease -> {
+                val intent: Intent = Intent(this, DiseaseActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_menu_drug -> {
+                val intent: Intent = Intent(this, DrugActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        doctor_drawer_layout.closeDrawer(GravityCompat.START)
+        return true    }
+}
---
+++ app/src/main/java/com/example/medicalrecord/activity/DrugActivity.kt
@@ -0,0 +1,42 @@
+package com.example.medicalrecord.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.example.medicalrecord.R
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_drug.*
+import kotlinx.android.synthetic.main.toolbar.*
+
+class DrugActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drug)
+        setSupportActionBar(toolbar)
+        setTitle(R.string.drug)
+
+        val toggle = ActionBarDrawerToggle(this, drug_drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drug_drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        drug_nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_menu_doctor -> {
+                val intent: Intent = Intent(this, DoctorActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_menu_disease -> {
+                val intent: Intent = Intent(this, DiseaseActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        drug_drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
